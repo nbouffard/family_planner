5654991bee2b8a19a442c3ce0da77344ecfff19a
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-08-09 22:36:13 +0200

    add event controller, add new, create and show event

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,26 @@
+class EventsController < ApplicationController
+  def new
+    @member = Member.find(params[:member_id])
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.member = Member.find(params[:member_id])
+    if @event.save
+      redirect_to member_event_path(@event), notice: 'Event created successfully'
+    else
+      render :new, alert: 'Event not created'
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :time, :date, :address, :vaccination, :flea_treament, :worm_treatment)
+  end
+end
