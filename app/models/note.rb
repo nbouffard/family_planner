908582fe1691356ea8c796ac455f5bd98728325e
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-08-12 15:19:13 +0200

    add note associations to models

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,4 @@
 class Note < ApplicationRecord
+  belongs_to :noteable, polymorphic: true
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_many :members, dependent: :destroy
   has_many :events, through: :members
+  has_many :notes, dependent: :destroy
 end
