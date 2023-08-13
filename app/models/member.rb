d4a731c61f14b8044f8a10a3872e0297d9a540a1
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-08-13 11:23:14 +0200

    add photo association to models

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,5 @@ class Member < ApplicationRecord
   has_many :events, dependent: :destroy
   accepts_nested_attributes_for :events, allow_destroy: true
   has_many :notes, as: :noteable, dependent: :destroy
+  has_one_attached :photo
 end
