fe6abc27e56a36e4929f6a097b05ce948c3e5a99
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-08-15 09:22:39 +0200

    add geocoder associations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :member
   has_many :notes, as: :noteable, dependent: :destroy
   has_many_attached :photos
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_091923) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_072013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_091923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["member_id"], name: "index_events_on_member_id"
   end
 
